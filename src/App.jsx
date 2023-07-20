1ade56bbff37edcf26b07c592b342769da0fcb4a
Nabila Athifah Zahra <126486610+nabilaazahra1743@users.noreply.github.com>
2023-07-20 15:28:45 +0700

    upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Todo from './component/Todo.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
